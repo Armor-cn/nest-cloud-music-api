0b2b40f8546a679d894a1723b473a9e11a18272b
wangchunming <1669487366@qq.com>
2019-12-17 18:48:11 +0800

    fix

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -4,6 +4,6 @@ export class BaseService {
     // private readonly cloudMusicApi = 'https://music.aityp.com/';
     // private readonly cloudMusicApi = 'http://localhost:3000/';
     // cloudMusicApi = 'https://music.aityp.com/';
-    cloudMusicApi ='http://129.28.201.249:3089/'
+     cloudMusicApi ='http://localhost:3089/'
 }
 
