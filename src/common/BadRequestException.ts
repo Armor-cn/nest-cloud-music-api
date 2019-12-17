df26cda6f3c768fb98488d74d3646e3147af6cfb
wangchunming <1669487366@qq.com>
2019-12-17 18:37:38 +0800

    fix

---
+++ src/common/BadRequestException.ts
@@ -0,0 +1,4 @@
+import { BaseException } from './BaseException';
+
+export class BadRequestException extends BaseException {
+}
--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -3,6 +3,7 @@ export class BaseService {
     // 网易云提供的 Api 接口http://musicapi.leanapp.cn/
     // private readonly cloudMusicApi = 'https://music.aityp.com/';
     // private readonly cloudMusicApi = 'http://localhost:3000/';
-    cloudMusicApi = 'https://music.aityp.com/';
+    // cloudMusicApi = 'https://music.aityp.com/';
+    cloudMusicApi ='http://129.28.201.249:3089/'
 }
 
