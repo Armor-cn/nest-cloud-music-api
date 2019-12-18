99d22b320edce8867f5e4a694eff1b4ec076d692
wangchunming <1669487366@qq.com>
2019-12-18 13:56:48 +0800

    fix:

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -3,7 +3,7 @@ export class BaseService {
     // 网易云提供的 Api 接口http://musicapi.leanapp.cn/
     // private readonly cloudMusicApi = 'https://music.aityp.com/';
     // cloudMusicApi = 'https://music.aityp.com/';
-    //  cloudMusicApi ='http://localhost:3089/'
-    cloudMusicApi = 'http://129.28.201.249:3089/';
+    cloudMusicApi = 'http://localhost:3089/'
+    // cloudMusicApi = 'http://129.28.201.249:3089/';
 }
 
